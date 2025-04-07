838248bc527d5d440a0bdeb80e65b5495f6df828
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-04-08 00:56:54 +0530

    Update udp_client.c

--- UDP/udp_client.c
+++ UDP/udp_client.c
@@ -5,46 +5,39 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define SERVER_PORT 4000
+#define PORT 4000
 #define MAX 1024
 
 int main() {
     int sockfd, n;
     char buffer[MAX];
-    struct sockaddr_in server_addr;
-    socklen_t server_len;
+    struct sockaddr_in serv_addr;
+    socklen_t addr_size;
 
-    // 1. Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Socket creation failed");
         exit(1);
     }
-    printf("UDP socket created. Client ready to send messages.\n");
+    printf("UDP socket created. Client ready to send and receive.\n");
 
-    // 2. Set up server address
-    bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
-    
-    server_len = sizeof(server_addr);
-    printf("Client ready to communicate with server on port %d...\n", SERVER_PORT);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP of the server
+
+    addr_size = sizeof(serv_addr);
 
     while (1) {
-        // 3. Send message to server
         printf("Enter message to server: ");
-        bzero(buffer, MAX);
         fgets(buffer, MAX, stdin);
-        
-        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, server_len);
-        
+
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&serv_addr, addr_size);
+
         if (strncmp(buffer, "quit", 4) == 0)
             break;
 
-        // 4. Receive reply from server
         bzero(buffer, MAX);
-        n = recvfrom(sockfd, buffer, MAX, 0, (struct sockaddr*)&server_addr, &server_len);
+        n = recvfrom(sockfd, buffer, MAX, 0, NULL, NULL);
         buffer[n] = '\0';
         printf("Received from server: %s\n", buffer);
     }
