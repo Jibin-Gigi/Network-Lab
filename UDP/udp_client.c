187a9605e5e94aaa344fc1baff671499c0840f94
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2025-04-08 00:48:17 +0530

    Update udp_client.c

--- UDP/udp_client.c
+++ UDP/udp_client.c
@@ -5,14 +5,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define PORT 4000
+#define SERVER_PORT 4000
 #define MAX 1024
 
 int main() {
     int sockfd, n;
     char buffer[MAX];
-    struct sockaddr_in cli_addr;
-    socklen_t addr_size;
+    struct sockaddr_in server_addr;
+    socklen_t server_len;
 
     // 1. Create UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -20,35 +20,33 @@ int main() {
         perror("Socket creation failed");
         exit(1);
     }
-    printf("UDP socket created. Client ready to receive.\n");
+    printf("UDP socket created. Client ready to send messages.\n");
 
-    // 2. Bind to local port
-    cli_addr.sin_family = AF_INET;
-    cli_addr.sin_port = htons(PORT);
-    cli_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr*)&cli_addr, sizeof(cli_addr)) < 0) {
-        perror("Bind failed");
-        close(sockfd);
-        exit(1);
-    }
-
-    addr_size = sizeof(cli_addr);
-    printf("Client is listening on port %d...\n", PORT);
+    // 2. Set up server address
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
+    
+    server_len = sizeof(server_addr);
+    printf("Client ready to communicate with server on port %d...\n", SERVER_PORT);
 
     while (1) {
-        // 3. Receive from server
-        n = recvfrom(sockfd, buffer, MAX, 0, (struct sockaddr*)&cli_addr, &addr_size);
-        buffer[n] = '\0';
-        printf("Received from server: %s\n", buffer);
-
+        // 3. Send message to server
+        printf("Enter message to server: ");
+        bzero(buffer, MAX);
+        fgets(buffer, MAX, stdin);
+        
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&server_addr, server_len);
+        
         if (strncmp(buffer, "quit", 4) == 0)
             break;
 
-        // 4. Reply to server
-        printf("Enter reply to server: ");
-        fgets(buffer, MAX, stdin);
-        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&cli_addr, addr_size);
+        // 4. Receive reply from server
+        bzero(buffer, MAX);
+        n = recvfrom(sockfd, buffer, MAX, 0, (struct sockaddr*)&server_addr, &server_len);
+        buffer[n] = '\0';
+        printf("Received from server: %s\n", buffer);
     }
 
     close(sockfd);
